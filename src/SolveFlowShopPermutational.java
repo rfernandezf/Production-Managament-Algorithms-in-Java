c1e5cf6ff664337cbc353e86a4c88b18b41ea2b9
Plata17 <pl4t417@gmail.com>
2016-10-04 18:22:16 +0200

    - Finished Permutation class - Finished Main class - Added SolveFlowShopPermutational class, that calculate the matrix with the solution

---
+++ src/SolveFlowShopPermutational.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Plata on 04/10/2016.
+ */
+public class SolveFlowShopPermutational {
+
+        private List<List<Integer>> inputMatrix = new ArrayList<List<Integer>>();
+        private List<List<Integer>> outputMatrix = new ArrayList<List<Integer>>();
+        private List<Integer> permutation = new ArrayList<Integer>();
+
+        public SolveFlowShopPermutational(MatrixFromFile inputMatrix, Permutation permutation){
+            this.inputMatrix = inputMatrix.getMatrix();
+            this.permutation = permutation.result();
+
+
+        }
+
+        public List<List<Integer>> result(){
+            return this.outputMatrix;
+        }
+}
