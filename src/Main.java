976a9a2ab0fb48c0133dc4f2631bb89c7f85660a
Plata17 <pl4t417@gmail.com>
2016-10-04 19:05:31 +0200

    Work on Solver class
    TO-DO:
    End solve class

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        int numberOfMachines = 2;
-        int numberOfOrders = 5;
 
 
         MatrixFromFile initialMatrix = new MatrixFromFile("rsc/ejem_clase1.txt");
+        System.out.print("Number of orders: " + initialMatrix.getOrderNum() + ".\n");
+        System.out.print("Number of machines: " + initialMatrix.getMachNum() + ".\n");
         System.out.println("Input matrix: " + initialMatrix.getInputMatrix());
 
-        Permutation permutation = new Permutation(numberOfOrders);
+        Permutation permutation = new Permutation(initialMatrix.getOrderNum());
         System.out.println("Permutation: " + permutation.result());
 
         SolveFlowShopPermutational outputMatrix = new SolveFlowShopPermutational(initialMatrix, permutation);
--- src/SolveFlowShopPermutational.java
+++ src/SolveFlowShopPermutational.java
@@ -9,12 +9,24 @@ public class SolveFlowShopPermutational {
         private List<List<Integer>> inputMatrix = new ArrayList<List<Integer>>();
         private List<List<Integer>> outputMatrix = new ArrayList<List<Integer>>();
         private List<Integer> permutation = new ArrayList<Integer>();
+        private int numberOfMachines;
+        private int numberOfOrders;
 
         public SolveFlowShopPermutational(MatrixFromFile inputMatrix, Permutation permutation){
             this.inputMatrix = inputMatrix.getInputMatrix();
+            this.numberOfMachines = inputMatrix.getMachNum();
+            this.numberOfOrders = inputMatrix.getOrderNum();
             this.permutation = permutation.result();
 
+            boolean processEnd = false;
+            List<Integer> machines = new ArrayList<Integer>();
+            for (int i = 0; i < this.numberOfMachines; i++){
+                machines.add(0);
+            }
 
+            while (processEnd == false){
+                processEnd=true;
+            }
 
         }
 
